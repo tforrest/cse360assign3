6c1de041b2f675780e5aa81e925f63da994e32a0
taforrest <taforres@asu.edu>
2016-02-22 13:37:28 -0700

    inital commit of cse360assign3

---
+++ src/cse360assign3/Calculator.java
@@ -0,0 +1,63 @@
+package cse360assign3;
+/**
+ * Calculator keeps a running total of an int
+ * by multiplying dividing adding and subtracting
+ * @author Thomas Forrest
+ *
+ */
+
+public class Calculator {
+	/**Keeps track of the total */
+	private int total;
+	
+	/**
+	 * Constructor for calculator
+	 * sets a totals int to 0
+	 * 
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the private int total
+	 * 
+	 * @return total 
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds an int value to total
+	 * 
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	/**
+	 * Subtracts an int value from total
+	 * 
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * 
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
